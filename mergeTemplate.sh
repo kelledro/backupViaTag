a9e6cf3358e39037148f35d7d16a30791a6744d7
kelledro <arkade@users.noreply.github.com>
2015-10-29 04:52:46 +0000

    add merge script

---
+++ mergeTemplate.sh
@@ -0,0 +1,5 @@
+#~/bin/bash
+ base64 backupViaTag | awk '{printf "\t\t\t\t\t\"%s\\n\",\n", $0}' > /tmp/backupViaTag.b64 && sed '/<<base64script>>/{
+    s/<<base64script>>//g
+    r /tmp/backupViaTag.b64
+}' backupViaTag.template.template
