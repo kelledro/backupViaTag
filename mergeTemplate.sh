1d0fcdf5dc21ee492613d509a54fe639b1a21231
kelledro <arkade@users.noreply.github.com>
2015-11-02 10:30:25 +0000

    shell script updatest

--- mergeTemplate.sh
+++ mergeTemplate.sh
@@ -1,5 +1,5 @@
 #~/bin/bash
- base64 backupViaTag | awk '{printf "\t\t\t\t\t\"%s\\n\",\n", $0}' > /tmp/backupViaTag.b64 && sed '/<<base64script>>/{
-    s/<<base64script>>//g
+base64 backupViaTag | awk '{printf "\t\t\t\t\t\"%s\\n\",\n", $0}' > /tmp/backupViaTag.b64 && sed '/<<base64script>>/{
+    s/<<base64script>>//
     r /tmp/backupViaTag.b64
-}' backupViaTag.template.template
+}' backupViaTag.template.template 
